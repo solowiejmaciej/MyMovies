9ba417df04057c1464bc9e82005284847d69a1ec
solowiejmaciej <solowiejmaciej@gmail.com>
2024-01-04 21:32:08 +0100

    MigrationsAdded

---
+++ backend/Migrations/20240104155803_Rate changed to double.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyMovies.Migrations
+{
+    /// <inheritdoc />
+    public partial class Ratechangedtodouble : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Rate",
+                table: "Movies",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Rate",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
---
+++ backend/Migrations/20240104191457_ExternaldAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyMovies.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExternaldAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ThirdPartyId",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThirdPartyId",
+                table: "Movies");
+        }
+    }
+}
