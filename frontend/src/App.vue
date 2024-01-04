e2a13456882c5198af4dc5ff13dee71ebe9ce032
solowiejmaciej <solowiejmaciej@gmail.com>
2024-01-04 21:07:56 +0100

    Frontend added

---
+++ frontend/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <button @click="addMovie">Dodaj</button>
+    <table>
+      <tr v-for="(movie, index) in movies" :key="index">
+        <td>{{ movie.title }}</td>
+        <td><button @click="editMovie(index)">Edytuj</button></td>
+        <td><button @click="deleteMovie(index)">Usuń</button></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      movies: [] // Tutaj będą przechowywane dane o filmach
+    }
+  },
+  created() {
+    axios.get('http://localhost:8080/api/movies')
+      .then(response => {
+        this.movies = response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  },
+  methods: {
+    addMovie() {
+      // Tutaj powinna być logika dodawania filmu
+    },
+    editMovie(index) {
+      // Tutaj powinna być logika edycji filmu
+    },
+    deleteMovie(index) {
+      if (confirm('Czy na pewno chcesz usunąć ten film?')) {
+        this.movies.splice(index, 1);
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
